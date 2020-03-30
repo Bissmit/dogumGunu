97828efc6111b2c15e9f0e743ff799f3f0066803
Bissmit <43hamzacelik@gmail.com>
2020-03-30 16:37:30 +0300

    "coming soon integre"

--- index.php
+++ index.php
@@ -1,101 +1,147 @@
-<!doctype html>
+
+<!DOCTYPE html>
 <html lang="en">
-  <head>
-    <!-- Required meta tags -->
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-
-    <!-- Custom CSS -->
-    <link rel="stylesheet" type="text/css" href="css/style.css">
-    <!-- Bootstrap CSS -->
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
-
-    <title>Doğum Günü!</title>
-  </head>
-  <body class="bg-warning text-center">
-
-
-    
-    <div class="ustmenu" style="padding-bottom: 2px; ">
-    	<div class="solResim float-right"></div>
-    	<div class="solResim float-left"></div>
-  		<h1 class="" style="margin-top: 20px; padding-top: 3px;"> Ali Emre KOCATÜRK!<br><span style=" background-image: url();margin-bottom: 0px; margin-top: 0px;" class="badge badge-info badge-pill">20</span> </h1>
-    </div>
-
-
-
-    <div class="container " style="background-image: url(img/glitter-gif-png-5-original.gif); padding-bottom: 10px;">
-
+<head>
+	<!-- Meta -->
+	<meta charset="utf-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<meta name="description" content="">
+    <meta name="keywords" content="">
+    <meta name="author" content="Awaiken Theme">
+    <meta name='robots' content='noindex,nofollow' />
+	<!-- Page Title -->
+	<title>Çok Yakında !</title>
+	<!-- Google Fonts css-->
+	<link href="https://fonts.googleapis.com/css?family=Roboto+Condensed:300,400,700" rel="stylesheet">
+	<!-- Bootstrap css -->
+	<link href="css/bootstrap.min.css" rel="stylesheet" media="screen">
+	<!-- Font Awesome icon css-->
+	<link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.11.2/css/all.css" rel="stylesheet" media="screen">
+	<!-- Main custom css -->
+	<link href="css/custom.css" rel="stylesheet" media="screen">
+	<!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
+    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+    <!--[if lt IE 9]>
+      <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+      <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+    <![endif]-->
+</head>
+<body style="background-image: url('https://i.pinimg.com/originals/11/a7/56/11a75676504c93cc32153c7c7aae23f6.gif');">
 	
-    	<img class="resim" src="img/pngguru.com.png" width="90%">
-
-		<div id="carouselExampleSlidesOnly" class="carousel slide" data-ride="carousel">
-		  <div class="carousel-inner">
-		    <div class="carousel-item active">
-		        <img src="img/slideFirst.png" class="d-block w-100" alt="...">
-		    </div>
-		    <div class="carousel-item">
-		      <img src="img/slideSecond.png" class="d-block w-100" alt="...">
-		    </div>
-		    <div class="carousel-item">
-		      <img src="img/slideThird.png" class="d-block w-100" alt="...">
-		    </div>
-		  </div>
+	<!-- Coming Soon Wrapper starts -->
+	<div class="comming-soon">
+		
+		<div class="container-fluid">
+			<div class="row">
+				<div class="col-md-12">
+					<div class="coming-soon-box">
+						<!-- Logo Start -->
+						<div style="color: white;">
+							<h1>Ali Emre KOCATÜRK</h1>
+							<p>En yakın zamanda sizlerle...</p>
+						</div>
+						<!-- Logo end -->
+						
+						<!-- Types Information start -->
+						<div class="coming-text">
+							<p>Sakin ol</p>
+							<h2><span class="typed-title">Yükleniyor...</span></h2>
+							<div class="typing-title">
+								<p> 1'Nisana çok az kaldı</p>
+								<p>Takipte kal</p>
+								<p>31 Mart 24:00 !</p>
+								<p>Burada Olacağız !</p>
+							</div>
+						</div>
+						<!-- Types Information end -->
+						
+						<!-- Countdown start -->
+						<div class="countdown-timer-wrapper">
+							<div class="timer" id="countdown"></div>
+						</div>
+						<!-- Countdown end -->
+						
+						<!-- kendi geri sayim basladi -->
+						<p id="demo"></p>
+						
+						<!-- kendi gerisayim bitti -->
+						
+						<!-- Social Media start -->
+						<div class="social-link">
+							<a href=""><i class="fab fa-facebook"></i></a>
+							<a href=""><i class="fab fa-twitter"></i></a>
+							<a href=""><i class="fab fa-linkedin"></i></a>
+							<a href=""><i class="fab fa-pinterest"></i></a>
+							<a href=""><i class="fab fa-instagram"></i></a>
+						</div>
+						<!-- Social Media end -->	
+					</div>
+				</div>
+			</div>
 		</div>
-
-
-		<img class="resim" src="img/celebrate.png" width="90%">
-
-		<div id="carouselExampleControls" class="carousel slide carousel-fade" data-ride="carousel">
-		  <div class="carousel-inner">
-		    <div class="carousel-item active">
-		      <img src="img/slideThird.png" class="d-block w-100" alt="...">
-		    </div>
-		    <div class="carousel-item">
-		      <img src="img/slideFirst.png" class="d-block w-100" alt="...">
-		    </div>
-		    <div class="carousel-item">
-		      <img src="img/slideSecond.png" class="d-block w-100" alt="...">
-		    </div>
-		  </div>
-		  <a class="carousel-control-prev" href="#carouselExampleControls" role="button" data-slide="prev">
-		    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-		    <span class="sr-only">Previous</span>
-		  </a>
-		  <a class="carousel-control-next" href="#carouselExampleControls" role="button" data-slide="next">
-		    <span class="carousel-control-next-icon" aria-hidden="true"></span>
-		    <span class="sr-only">Next</span>
-		  </a>
-		</div>
-    </div>
-    <blockquote class="blockquote text-center ">
-  		<p class="mb-0">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer posuere erat a ante.</p>
-  		<footer class="blockquote-footer"><cite title="Senin İçin Yazdı">Mıstıfa SEZİŞLİ</cite></footer>
-	</blockquote>
-    <blockquote class="blockquote text-center">
-  		<p class="mb-0">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer posuere erat a ante.</p>
-  		<footer class="blockquote-footer"><cite title="Senin İçin Yazdı">İnes CEYLAN</cite></footer>
-	</blockquote>
-	<blockquote class="blockquote text-center">
-  		<p class="mb-0">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer posuere erat a ante.</p>
-  		<footer class="blockquote-footer"><cite title="Senin İçin Yazdı">Selim ERVÜZ</cite></footer>
-	</blockquote>
-	    <blockquote class="blockquote text-center">
-  		<p class="mb-0">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer posuere erat a ante.</p>
-  		<footer class="blockquote-footer"><cite title="Senin İçin Yazdı">Hamza ÇELİK</cite></footer>
-	</blockquote>
-	
-	<div class="alt">
-		<div class="container text-light">
-			<p>Bu site Ali Emre KOCATÜRK'için en best arkadaşları tarafından yapılmıştır.<br>Nice mutlu yıllara...</p>
-			<img class="resim" src="img/fire.gif" width="90%">
-		</div>		
 	</div>
+	<!-- Coming Soon Wrapper end -->
 
-    <!-- Optional JavaScript -->
-    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
-    <script src="https://code.jquery.com/jquery-3.4.1.slim.min.js" integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous"></script>
-    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
-    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script>
-  </body>
-</html>
+    <!-- Jquery Library File -->
+	<script src="js/jquery-1.12.4.min.js"></script>
+	<!-- Timer counter js file -->
+	<script src="js/countdown-timer.js"></script>
+	<!-- Typed js file -->
+	<script src="js/typed.js"></script>
+	<!-- SmoothScroll -->
+	<script src="js/SmoothScroll.js"></script>
+    <!-- Bootstrap js file -->
+	<script src="js/bootstrap.min.js"></script>
+    <!-- Main Custom js file -->
+	<script src="js/function.js"></script>
+	<!-- Timer counter start -->
+	<script>
+        $(document).ready(function (){
+			var myDate = new Date("Mar 30, 2020 14:27:00");
+			//var myDate = new Date();vv
+			myDate.setDate(myDate.getDate());
+            $("#countdown").countdown(myDate, function (event) {
+                $(this).html(
+                    event.strftime(
+                        '<div class="timer-wrapper"><div class="time">%D</div><span class="text">Gün</span></div><div class="timer-wrapper"><div class="time">%H</div><span class="text">Saat</span></div><div class="timer-wrapper"><div class="time">%M</div><span class="text">Dakika</span></div><div class="timer-wrapper"><div class="time">%S</div><span class="text">Saniye</span></div>'
+                    )
+                );
+            });
+
+        });
+    </script>
+
+    <script>
+// Set the date we're counting down to
+var countDownDate = new Date("Mar 30, 2020 14:27:00").getTime();
+
+// Update the count down every 1 second
+var x = setInterval(function() {
+
+  // Get today's date and time
+  var now = new Date().getTime();
+
+  // Find the distance between now and the count down date
+  var distance = countDownDate - now;
+
+  // Time calculations for days, hours, minutes and seconds
+  var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+  var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+  var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+  var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+  // Display the result in the element with id="demo"
+  document.getElementById("demo").innerHTML = days + "d " + hours + "h "
+  + minutes + "m " + seconds + "s ";
+
+  // If the count down is finished, write some text
+  if (distance < 0) {
+    clearInterval(x);
+    document.getElementById("demo").innerHTML = "Beklenen an geldi, 3sn içinde yönlendiriliyorsunuz...";
+    window.setTimeout(function(){location.href = 'giris/';}, 3000); 
+  }
+}, 1000);
+</script>
+	<!-- Timer counter end -->
+</body>
