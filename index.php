7dd2aa7b5c5ec7caf16bbac58367dc3104c574ef
0xHamza <62814321+0xHamza@users.noreply.github.com>
2021-03-23 17:09:16 +0300

    update birthday

--- index.php
+++ index.php
@@ -1,3 +1,5 @@
+
+
 <!DOCTYPE html>
 <html lang="tr">
 <head>
@@ -132,7 +134,7 @@
 
     <script>
 // Set the date we're counting down to
-var countDownDate = new Date("Mar 31, 2020 24:00:00").getTime();
+var countDownDate = new Date("Mar 31, 2021 24:00:00").getTime();
 
 // Update the count down every 1 second
 var x = setInterval(function() {
